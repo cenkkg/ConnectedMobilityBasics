7e00769af15d87a317af2d0bb5239b3cb18c5dc9
DorukGercel <dorukgercel@hotmail.com>
2023-12-11 22:28:56 +0100

    Fix sim time

--- src/movement/ProhibitedFMIBuilding.java
+++ src/movement/ProhibitedFMIBuilding.java
@@ -19,16 +19,20 @@ import java.util.*;
  */
 public class ProhibitedFMIBuilding
         extends MapBasedMovement {
-
-    public static final int SIM_TIME = 6000;
+    public static final int SIM_TIME = 43000;
     public static final int SCHEDULE_LEN = 10;
     //==========================================================================//
     // Settings
     //==========================================================================//
-    /** {@code true} to confine nodes inside the polygon */
+    /**
+     * {@code true} to confine nodes inside the polygon
+     */
+    public static final String SIM_TIME_SETTING = "endTime";
     public static final String INVERT_SETTING = "rwpInvert";
     public static final boolean INVERT_DEFAULT = false;
-    /** Per node group setting used for selecting a route file ({@value}) */
+    /**
+     * Per node group setting used for selecting a route file ({@value})
+     */
     public static final String ROUTE_FILE_S = "routeFile";
     public static final String CLASSROOMS_FILE_S = "classroomsFile";
     //==========================================================================//
@@ -37,92 +41,28 @@ public class ProhibitedFMIBuilding
     //==========================================================================//
     // Instance vars
     //==========================================================================//
-    /** Prototype's reference to all routes read for the group */
+    /**
+     * Prototype's reference to all routes read for the group
+     */
+    private int simTime;
     private List<Coord> polygon;
     private List<Coord> classrooms;
     private Coord lastWaypoint;
     private List<Integer> schedule;
-    private int simTime = 3600;
-    /** Inverted, i.e., only allow nodes to move inside the polygon. */
-    private final boolean invert;
-    /** sim map for the model */
+    /**
+     * Inverted, i.e., only allow nodes to move inside the polygon.
+     */
+    private boolean invert;
+    /**
+     * sim map for the model
+     */
     private SimMap map = null;
     //==========================================================================//
 
-
-
-    //==========================================================================//
-    // Implementation
-    //==========================================================================//
-    @Override
-    public Path getPath() {
-        // Creates a new path from the previous waypoint to a new one.
-        final Path p;
-        p = new Path( super.generateSpeed() );
-        p.addWaypoint( this.lastWaypoint.clone() );
-        // Add only one point. An arbitrary number of Coords could be added to
-        // the path here and the simulator will follow the full path before
-        // asking for the next one.
-        Coord c;
-        if(SimClock.getIntTime() > SIM_TIME) {
-            p.addWaypoint( this.lastWaypoint.clone() );
-            return p;
-        }
-        int period = SimClock.getIntTime()/(SIM_TIME/SCHEDULE_LEN);
-        if(schedule.get(period) >= 0) {
-            c = classrooms.get(schedule.get(period));
-            double verMov = c.getY() - this.lastWaypoint.getY();
-            double horMov = c.getX() - this.lastWaypoint.getX();
-            boolean isFirstPass = true;
-            while ( pathIntersects( this.polygon, this.lastWaypoint, c ) ) {
-                if(isFirstPass) {
-                    c = new Coord(this.lastWaypoint.getX()+horMov, this.lastWaypoint.getY());
-                } else {
-                    c = new Coord(this.lastWaypoint.getX(), this.lastWaypoint.getY()+verMov);
-                }
-                isFirstPass = false;
-            }
-        } else {
-            do {
-                c = this.randomCoord();
-            } while ( pathIntersects( this.polygon, this.lastWaypoint, c ) );
-        }
-        p.addWaypoint( c );
-
-        this.lastWaypoint = c;
-        return p;
-    }
-
-    @Override
-    public Coord getInitialLocation() {
-        do {
-            this.lastWaypoint = this.randomCoord();
-        } while ( ( this.invert ) ?
-                isOutside( polygon, this.lastWaypoint ) :
-                isInside( this.polygon, this.lastWaypoint ) );
-        System.out.println(this.lastWaypoint);
-        return this.lastWaypoint;
-    }
-
-    @Override
-    public ProhibitedFMIBuilding replicate() {
-        return new ProhibitedFMIBuilding( this );
-    }
-
-    private Coord randomCoord() {
-        return new Coord(
-                rng.nextDouble() * super.getMaxX(),
-                rng.nextDouble() * super.getMaxY() );
-    }
-    //==========================================================================//
-
-
-    //==========================================================================//
-    // API
-    //==========================================================================//
-    public ProhibitedFMIBuilding( final Settings settings ) {
-        super( settings );
-        this.invert = settings.getBoolean( INVERT_SETTING, INVERT_DEFAULT );
+    public ProhibitedFMIBuilding(final Settings settings) {
+        super(settings);
+        this.simTime = settings.getInt(SIM_TIME_SETTING, SIM_TIME);
+        this.invert = settings.getBoolean(INVERT_SETTING, INVERT_DEFAULT);
         String buildingFileName = settings.getSetting(ROUTE_FILE_S);
         String classroomsFileName = settings.getSetting(CLASSROOMS_FILE_S);
         WKTReader reader = new WKTReader();
@@ -143,59 +83,124 @@ public class ProhibitedFMIBuilding
 
     }
 
-    public ProhibitedFMIBuilding( final ProhibitedFMIBuilding other ) {
+    public ProhibitedFMIBuilding(final ProhibitedFMIBuilding other) {
         // Copy constructor will be used when settings up nodes. Only one
         // prototype node instance in a group is created using the Settings
         // passing constructor, the rest are replicated from the prototype.
-        super( other );
+        super(other);
         // Remember to copy any state defined in this class.
         this.invert = other.invert;
         this.polygon = other.polygon;
         this.schedule = other.schedule;
         this.classrooms = other.classrooms;
+        this.simTime = other.simTime;
     }
-    //==========================================================================//
 
 
     //==========================================================================//
-    // Private - geometry
+    // Implementation
     //==========================================================================//
+    @Override
+    public Path getPath() {
+        // Creates a new path from the previous waypoint to a new one.
+        final Path p;
+        p = new Path(super.generateSpeed());
+        p.addWaypoint(this.lastWaypoint.clone());
+        // Add only one point. An arbitrary number of Coords could be added to
+        // the path here and the simulator will follow the full path before
+        // asking for the next one.
+        Coord c;
+        if (SimClock.getIntTime() > this.simTime) {
+            p.addWaypoint(this.lastWaypoint.clone());
+            return p;
+        }
+        int period = SimClock.getIntTime() / (this.simTime / SCHEDULE_LEN);
+        System.out.println(this.simTime / SCHEDULE_LEN);
+        System.out.println(period);
+        if (schedule.get(period) >= 0) {
+            System.out.println("a");
+            c = classrooms.get(schedule.get(period));
+            double verMov = c.getY() - this.lastWaypoint.getY();
+            double horMov = c.getX() - this.lastWaypoint.getX();
+            boolean isFirstPass = true;
+            while (pathIntersects(this.polygon, this.lastWaypoint, c)) {
+                if (isFirstPass) {
+                    c = new Coord(this.lastWaypoint.getX() + horMov, this.lastWaypoint.getY());
+                } else {
+                    c = new Coord(this.lastWaypoint.getX(), this.lastWaypoint.getY() + verMov);
+                }
+                isFirstPass = false;
+            }
+        } else {
+            System.out.println("b");
+            do {
+                c = this.randomCoord();
+            } while (pathIntersects(this.polygon, this.lastWaypoint, c));
+        }
+        p.addWaypoint(c);
+
+        this.lastWaypoint = c;
+        return p;
+    }
+
+    @Override
+    public Coord getInitialLocation() {
+        do {
+            this.lastWaypoint = this.randomCoord();
+        } while ((this.invert) ?
+                isOutside(polygon, this.lastWaypoint) :
+                isInside(this.polygon, this.lastWaypoint));
+        System.out.println(this.lastWaypoint);
+        return this.lastWaypoint;
+    }
+
+    @Override
+    public ProhibitedFMIBuilding replicate() {
+        return new ProhibitedFMIBuilding(this);
+    }
+
+    private Coord randomCoord() {
+        return new Coord(
+                rng.nextDouble() * super.getMaxX(),
+                rng.nextDouble() * super.getMaxY());
+    }
+
     private static boolean pathIntersects(
-            final List <Coord> polygon,
+            final List<Coord> polygon,
             final Coord start,
-            final Coord end ) {
-        final int count = countIntersectedEdges( polygon, start, end );
-        return ( count > 0 );
+            final Coord end) {
+        final int count = countIntersectedEdges(polygon, start, end);
+        return (count > 0);
     }
 
     private static boolean isInside(
-            final List <Coord> polygon,
-            final Coord point ) {
-        final int count = countIntersectedEdges( polygon, point,
-                new Coord( -10,0 ) );
-        return ( ( count % 2 ) != 0 );
+            final List<Coord> polygon,
+            final Coord point) {
+        final int count = countIntersectedEdges(polygon, point,
+                new Coord(-10, 0));
+        return ((count % 2) != 0);
     }
 
     private static boolean isOutside(
-            final List <Coord> polygon,
-            final Coord point ) {
-        return !isInside( polygon, point );
+            final List<Coord> polygon,
+            final Coord point) {
+        return !isInside(polygon, point);
     }
 
     private static int countIntersectedEdges(
-            final List <Coord> polygon,
+            final List<Coord> polygon,
             final Coord start,
-            final Coord end ) {
+            final Coord end) {
         int count = 0;
-        for ( int i = 0; i < polygon.size() - 1; i++ ) {
-            final Coord polyP1 = polygon.get( i );
-            final Coord polyP2 = polygon.get( i + 1 );
+        for (int i = 0; i < polygon.size() - 1; i++) {
+            final Coord polyP1 = polygon.get(i);
+            final Coord polyP2 = polygon.get(i + 1);
 
-            final Coord intersection = intersection( start, end, polyP1, polyP2 );
-            if ( intersection == null ) continue;
+            final Coord intersection = intersection(start, end, polyP1, polyP2);
+            if (intersection == null) continue;
 
-            if ( isOnSegment( polyP1, polyP2, intersection )
-                    && isOnSegment( start, end, intersection ) ) {
+            if (isOnSegment(polyP1, polyP2, intersection)
+                    && isOnSegment(start, end, intersection)) {
                 count++;
             }
         }
@@ -205,21 +210,21 @@ public class ProhibitedFMIBuilding
     private static boolean isOnSegment(
             final Coord L0,
             final Coord L1,
-            final Coord point ) {
+            final Coord point) {
         final double crossProduct
-                = ( point.getY() - L0.getY() ) * ( L1.getX() - L0.getX() )
-                - ( point.getX() - L0.getX() ) * ( L1.getY() - L0.getY() );
-        if ( Math.abs( crossProduct ) > 0.0000001 ) return false;
+                = (point.getY() - L0.getY()) * (L1.getX() - L0.getX())
+                - (point.getX() - L0.getX()) * (L1.getY() - L0.getY());
+        if (Math.abs(crossProduct) > 0.0000001) return false;
 
         final double dotProduct
-                = ( point.getX() - L0.getX() ) * ( L1.getX() - L0.getX() )
-                + ( point.getY() - L0.getY() ) * ( L1.getY() - L0.getY() );
-        if ( dotProduct < 0 ) return false;
+                = (point.getX() - L0.getX()) * (L1.getX() - L0.getX())
+                + (point.getY() - L0.getY()) * (L1.getY() - L0.getY());
+        if (dotProduct < 0) return false;
 
         final double squaredLength
-                = ( L1.getX() - L0.getX() ) * ( L1.getX() - L0.getX() )
-                + (L1.getY() - L0.getY() ) * (L1.getY() - L0.getY() );
-        if ( dotProduct > squaredLength ) return false;
+                = (L1.getX() - L0.getX()) * (L1.getX() - L0.getX())
+                + (L1.getY() - L0.getY()) * (L1.getY() - L0.getY());
+        if (dotProduct > squaredLength) return false;
 
         return true;
     }
@@ -228,24 +233,25 @@ public class ProhibitedFMIBuilding
             final Coord L0_p0,
             final Coord L0_p1,
             final Coord L1_p0,
-            final Coord L1_p1 ) {
-        final double[] p0 = getParams( L0_p0, L0_p1 );
-        final double[] p1 = getParams( L1_p0, L1_p1 );
-        final double D = p0[ 1 ] * p1[ 0 ] - p0[ 0 ] * p1[ 1 ];
-        if ( D == 0.0 ) return null;
+            final Coord L1_p1) {
+        final double[] p0 = getParams(L0_p0, L0_p1);
+        final double[] p1 = getParams(L1_p0, L1_p1);
+        final double D = p0[1] * p1[0] - p0[0] * p1[1];
+        if (D == 0.0) return null;
 
-        final double x = ( p0[ 2 ] * p1[ 1 ] - p0[ 1 ] * p1[ 2 ] ) / D;
-        final double y = ( p0[ 2 ] * p1[ 0 ] - p0[ 0 ] * p1[ 2 ] ) / D;
+        final double x = (p0[2] * p1[1] - p0[1] * p1[2]) / D;
+        final double y = (p0[2] * p1[0] - p0[0] * p1[2]) / D;
 
-        return new Coord( x, y );
+        return new Coord(x, y);
     }
 
     private static double[] getParams(
             final Coord c0,
-            final Coord c1 ) {
+            final Coord c1) {
         final double A = c0.getY() - c1.getY();
         final double B = c0.getX() - c1.getX();
         final double C = c0.getX() * c1.getY() - c0.getY() * c1.getX();
-        return new double[] { A, B, C };
+        return new double[]{A, B, C};
     }
+
 }
