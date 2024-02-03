d61c7af2deb884e9a93fd16238e6a2dc8e5611fb
DorukGercel <dorukgercel@hotmail.com>
2024-02-03 15:44:16 +0100

    Add csv extension

--- Assignment3/prob_selector.py
+++ Assignment3/prob_selector.py
@@ -1,13 +1,14 @@
+import csv
 import json
 import random
 
 import jmespath
 
 probe_dict = {
-    'home': ['./home_probes.json', './home_probes_100.json'],
-    'lte': ['./lte_probes.json', './lte_probes_100.json'],
-    'starlink': ['./starlink_probes.json', './starlink_probes_100.json'],
-    'wlan': ['./wlan_probes.json', './wlan_probes_100.json']
+    'home': ['./home_probes.json', './home_probes_100.json', './home_probes_ids_100.csv'],
+    'lte': ['./lte_probes.json', './lte_probes_100.json', './lte_probes_ids_100.csv'],
+    'starlink': ['./starlink_probes.json', './starlink_probes_100.json', './starlink_probes_ids_100.csv'],
+    'wlan': ['./wlan_probes.json', './wlan_probes_100.json', './wlan_probes_ids_100.csv']
 }
 
 
@@ -20,11 +21,15 @@ def probe_selector():
             with open(v[0], 'w') as w_file:
                 w_file.write(json.dumps(filtered_probes))
             n_no_probes = []
+            n_no_probes_ids = []
             rand_no = random.sample(range(0, len(filtered_probes)), min(100, len(filtered_probes)))
             for el in rand_no:
                 n_no_probes.append(filtered_probes[el])
+                n_no_probes_ids.append(str(filtered_probes[el]['id']))
             with open(v[1], 'w') as w_file:
                 w_file.write(json.dumps(n_no_probes))
+            with open(v[2], 'w') as w_file:
+                w_file.write(",".join(n_no_probes_ids))
 
 
 if __name__ == "__main__":
